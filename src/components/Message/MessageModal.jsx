c761a41728330fb34382365ed9f5c159cc3ce183
gyeong3un2 <gyeong3un2@gmail.com>
2023-12-11 17:58:35 +0900

    feat: 모달에서 메시지 내용과 작성자 명 입력하는 기능 구현
    +) NanumPen 폰트 적용

--- src/components/Message/MessageModal.jsx
+++ src/components/Message/MessageModal.jsx
@@ -4,7 +4,19 @@ import messageModal from '../../assets/images/messageModal.png';
 export default function MessageModal() {
   return (
     <div className="flex items-center justify-center ">
-      <img src={messageModal} alt="message modal" className="w-3/12" />
+      <img src={messageModal} alt="message modal" className="w-80" />
+      <input
+        type="text"
+        placeholder="작성자"
+        className="absolute w-64 p-1 text-xl border top-28 left-1/2 font-nanumPen"
+        style={{ left: '50%', transform: 'translate(-50%, -50%)' }}
+      />
+      
+      <textarea
+        placeholder="내용"
+        className="absolute w-64 h-40 p-1 text-xl border top-56 left-1/2 font-nanumPen"
+        style={{ left: '50%', transform: 'translate(-50%, -50%)' }}
+      />
     </div>
   );
 }

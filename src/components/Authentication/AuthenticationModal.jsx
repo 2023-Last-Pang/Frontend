d9dda2d645696b159171c6e877cd1c0c5cffb941
gyeong3un2 <gyeong3un2@gmail.com>
2023-12-20 22:23:47 +0900

    design: 인증코드 디자인 수정

--- src/components/Authentication/AuthenticationModal.jsx
+++ src/components/Authentication/AuthenticationModal.jsx
@@ -7,9 +7,9 @@ import InputAuth from './InputAuth';
 export default function AuthenticationModal({ handleOpenAuthentication }) {
   return (
     <div className="fixed top-0 left-0 z-10 flex items-center justify-center w-full h-screen text-center bg-black bg-opacity-40">
-      <div className="flex flex-col justify-between h-40 p-3 bg-white rounded-md w-96">
+      <div className="flex flex-col justify-between h-40 p-3 bg-white rounded-md w-84">
         <div className='flex flex-col items-center p-5'>
-          <div className="w-72">
+          <div className="mr-2 text-sm w-72">
             <p>슬랙 채널에 공유된 인증코드를 입력해주세요</p>
           </div>
           <InputAuth 

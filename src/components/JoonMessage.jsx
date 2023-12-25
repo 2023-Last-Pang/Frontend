d329b1afb2018070d703de5d7361a53cc6831230
woohee <aldkgha@naver.com>
2023-12-25 15:24:08 +0900

    feat: 슬라이더 모달 잘리는 문제 해결

--- src/components/JoonMessage.jsx
+++ src/components/JoonMessage.jsx
@@ -9,7 +9,6 @@ function AndrewMessage() {
   const handleImageClick = () => {
     setIsClicked(true);
 
-    // 2초 후에 메시지 숨기기
     setTimeout(() => {
       setIsClicked(false);
     }, 2000);
@@ -24,14 +23,13 @@ function AndrewMessage() {
         onClick={handleImageClick}
         className="relative w-96 cursor-pointer">
         {' '}
-        {/* 트리 이미지의 너비 조정 */}
         <img src={snowman1} alt="Snowman" className="w-38 h-60" />
         {isClicked && (
           <div
             className="absolute right-[90%] top-[-60%] z-20 translate-x-full transform"
             style={{
-              width: '300px', // Tailwind CSS로 너비 조정
-              height: '300px', // Tailwind CSS로 높이 조정
+              width: '300px',
+              height: '300px',
             }}>
             <img src={message} alt="Message" className="h-full w-full" />
             <p className="font-nanumPen absolute left-1/2 top-[43%] z-30 -translate-x-1/2 -translate-y-1/2 transform text-sm text-black">

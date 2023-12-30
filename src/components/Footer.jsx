41c04beab740528ce04c8be4623d97b004420879
gyeong3un2 <gyeong3un2@gmail.com>
2023-12-31 05:17:15 +0900

    fix: footer darkmode 없앰

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © [Techeer] Team Joon . All Rights Reserved.
         </span>
-        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0 dark:text-gray-400">
+        <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 sm:mt-0">
           <li>
             <a
               href="https://team-joon.oopy.io/"

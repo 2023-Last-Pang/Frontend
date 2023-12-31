e6270f13d264db71da473b0596afd1fbdc18c1f1
jhkim <wellsy001012@gmail.com>
2023-12-31 21:24:09 +0900

    fix: 컨페티테스트

--- src/components/ClockTest.jsx
+++ src/components/ClockTest.jsx
@@ -36,7 +36,7 @@ function ClockTest({ setViewMessageModal }) {
   const calculateTimeDifference = () => {
     const now = currentTime;
     // const newYear = moment('2024-1-1 00:00:00').tz('Asia/Seoul');
-    const newYear = moment('2023-12-31 21:23:30').tz('Asia/Seoul');
+    const newYear = moment('2023-12-31 21:25:00').tz('Asia/Seoul');
     const diff = newYear - now;
 
     // D-DAY 시간
